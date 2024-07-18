8693ed953eb543c9ce70c6b1f0be13ef9575d95d
Shane Celis <shane.celis@gmail.com>
2024-07-18 00:17:21 -0400

    feature: Initial code. A confirm prompt.

---
+++ examples/basic.rs
@@ -0,0 +1,137 @@
+use bevy::prelude::*;
+use bevy_asky::*;
+
+use bevy::{
+    color::palettes::css::GOLD,
+    diagnostic::{DiagnosticsStore, FrameTimeDiagnosticsPlugin},
+};
+
+fn main() {
+    App::new()
+        .add_plugins((DefaultPlugins, FrameTimeDiagnosticsPlugin, AskyPlugin))
+        .add_systems(Startup, setup)
+        .add_systems(Update, (text_update_system, text_color_system))
+        .run();
+}
+
+// A unit struct to help identify the FPS UI component, since there may be many Text components
+#[derive(Component)]
+struct FpsText;
+
+// A unit struct to help identify the color-changing Text component
+#[derive(Component)]
+struct ColorText;
+
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    // UI camera
+    commands.spawn(Camera2dBundle::default());
+    // Text with one section
+    commands.spawn((
+        // Create a TextBundle that has a Text with a single section.
+        TextBundle::from_section(
+            // Accepts a `String` or any type that converts into a `String`, such as `&str`
+            "hello\nbevy!",
+            TextStyle {
+                // This font is loaded and will be used instead of the default font.
+                font_size: 100.0,
+                ..default()
+            },
+        ) // Set the justification of the Text
+        .with_text_justify(JustifyText::Center)
+        // Set the style of the TextBundle itself.
+        .with_style(Style {
+            position_type: PositionType::Absolute,
+            bottom: Val::Px(5.0),
+            right: Val::Px(5.0),
+            ..default()
+        }),
+        ColorText,
+    ));
+
+    // Text with multiple sections
+    // commands.spawn((
+    //     // Create a TextBundle that has a Text with a list of sections.
+    //     TextBundle::from_sections([
+    //         TextSection::new(
+    //             "FPS: ",
+    //             TextStyle {
+    //                 // This font is loaded and will be used instead of the default font.
+    //                 font_size: 60.0,
+    //                 ..default()
+    //             },
+    //         ),
+    //         TextSection::from_style(
+    //             TextStyle {
+    //                 font_size: 60.0,
+    //                 color: GOLD.into(),
+    //                 // If no font is specified, the default font (a minimal subset of FiraMono) will be used.
+    //                 ..default()
+    //             }
+    //         ),
+    //     ]),
+    //     FpsText,
+    // ));
+
+    commands.spawn(
+        // Here we are able to call the `From` method instead of creating a new `TextSection`.
+        // This will use the default font (a minimal subset of FiraMono) and apply the default styling.
+        (
+        //  .with_style(
+        //     // Style {
+        //     //     position_type: PositionType::Absolute,
+        //     //     bottom: Val::Px(25.0),
+        //     //     left: Val::Px(15.0),
+        //     //     ..default()
+        //     // },
+        // ),
+        TextBundle::from(""),
+            AskyState::default(),
+            Confirm { message: "Do thing? ".into(), init: None },
+            // InheritedVisibility::VISIBLE,
+
+        ),
+    ).observe(|trigger: Trigger<AskyEvent<bool>>| {
+        eprintln!("got trigger for {:?}", trigger.event());
+    });
+
+    commands.spawn(
+        // Here we are able to call the `From` method instead of creating a new `TextSection`.
+        // This will use the default font (a minimal subset of FiraMono) and apply the default styling.
+        TextBundle::from("From an &str into a TextBundle with the default font!").with_style(
+            Style {
+                position_type: PositionType::Absolute,
+                bottom: Val::Px(5.0),
+                left: Val::Px(15.0),
+                ..default()
+            },
+        ),
+    );
+
+}
+
+fn text_color_system(time: Res<Time>, mut query: Query<&mut Text, With<ColorText>>) {
+    for mut text in &mut query {
+        let seconds = time.elapsed_seconds();
+
+        // Update the color of the first and only section.
+        text.sections[0].style.color = Color::srgb(
+            (1.25 * seconds).sin() / 2.0 + 0.5,
+            (0.75 * seconds).sin() / 2.0 + 0.5,
+            (0.50 * seconds).sin() / 2.0 + 0.5,
+        );
+    }
+}
+
+fn text_update_system(
+    diagnostics: Res<DiagnosticsStore>,
+    mut query: Query<&mut Text, With<FpsText>>,
+) {
+    for mut text in &mut query {
+        if let Some(fps) = diagnostics.get(&FrameTimeDiagnosticsPlugin::FPS) {
+            if let Some(value) = fps.smoothed() {
+                // Update the value of the second section
+                text.sections[1].value = format!("{value:.2}");
+            }
+        }
+    }
+}
---
+++ src/lib.rs
@@ -0,0 +1,167 @@
+use bevy::prelude::*;
+use std::borrow::Cow;
+
+pub struct AskyPlugin;
+
+impl Plugin for AskyPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(Update, run_confirm);
+    }
+}
+
+fn run_confirm(mut query: Query<(Entity, &mut AskyState, &Confirm, Option<&mut ConfirmState>)>,
+               mut text: Query<&mut Text>,
+               input: Res<ButtonInput<KeyCode>>,
+               mut commands: Commands) {
+    for (id, mut state, confirm, mut confirm_state) in query.iter_mut() {
+        match *state {
+            AskyState::Frozen => {
+            }
+            AskyState::Uninit => {
+            let mut display = None;
+            commands.entity(id)
+                .with_children(|parent| {
+                    parent.spawn(TextBundle::from(confirm.message.as_ref()));
+                    display = Some(parent.spawn(TextBundle::from(match confirm.init {
+                        Some(true) => "Y/n",
+                        Some(false) => "y/N",
+                        None => "y/n"
+                    })).id());
+
+                })
+                .insert(ConfirmState { display: display.unwrap(),
+                                        yes: None });
+
+                *state = AskyState::Reading;
+            }
+            AskyState::Reading => {
+
+                if let Some(ref mut confirm_state) = confirm_state {
+                    if input.any_just_pressed([KeyCode::KeyY, KeyCode::KeyN, KeyCode::Enter]) {
+                        if input.just_pressed(KeyCode::KeyY) {
+                            confirm_state.yes = Some(true);
+                        }
+                        if input.just_pressed(KeyCode::KeyN) {
+                            confirm_state.yes = Some(false);
+                        }
+                        let mut text  = text.get_mut(confirm_state.display).unwrap();
+                        text.sections[0] = match confirm_state.yes {
+                            Some(true) => "Y/n",
+                            Some(false) => "y/N",
+                            None => "y/n",
+                        }.into();
+
+                        if input.just_pressed(KeyCode::Enter) && confirm_state.yes.is_some() {
+                            commands.trigger_targets(AskyEvent(Ok(confirm_state.yes.unwrap())), id);
+                            *state = AskyState::Complete;
+                            text.sections[0] = if confirm_state.yes.unwrap() {
+                                "Yes"
+                            } else {
+                                "No"
+                            }.into();
+                        }
+                    }
+                } else {
+                    panic!("cannot get start while reading.");
+                }
+            }
+            AskyState::Complete | AskyState::Error => {
+            }
+        }
+        // if let Some(ref mut confirm_state) = confirm_state {
+        //     if input.any_just_pressed([KeyCode::KeyY, KeyCode::KeyN, KeyCode::Enter]) {
+        //         if input.just_pressed(KeyCode::KeyY) {
+        //             confirm_state.yes = Some(true);
+        //         }
+        //         if input.just_pressed(KeyCode::KeyN) {
+        //             confirm_state.yes = Some(false);
+        //         }
+        //         let mut text  = text.get_mut(confirm_state.display).unwrap();
+        //         text.sections[0] = match confirm_state.yes {
+        //             Some(true) => "Y/n",
+        //             Some(false) => "y/N",
+        //             None => "y/n",
+        //         }.into();
+
+        //         if input.just_pressed(KeyCode::Enter) && confirm_state.yes.is_some() {
+        //             commands.trigger_targets(AskyEvent(Ok(confirm_state.yes.unwrap())), id);
+        //         }
+        //     }
+        // } else {
+        // }
+    }
+}
+
+#[derive(Event, Deref, Debug)]
+pub struct AskyEvent<T>(Result<T, Error>);
+
+#[derive(Component)]
+pub struct Confirm {
+    /// Message used to display in the prompt.
+    pub message: Cow<'static, str>,
+    /// Initial confirm_state of the prompt.
+    pub init: Option<bool>,
+}
+
+#[derive(Debug, Component, Default)]
+pub enum AskyState {
+    Frozen,
+    #[default]
+    Uninit,
+    Reading,
+    Complete,
+    Error
+}
+
+
+#[derive(Component)]
+struct ConfirmState {
+    pub display: Entity,
+    pub yes: Option<bool>,
+}
+
+/// Asky errors
+#[derive(Debug, thiserror::Error)]
+pub enum Error {
+    /// User cancelled.
+    #[error("cancelled")]
+    Cancel,
+    /// Input was invalid.
+    #[error("invalid input")]
+    InvalidInput,
+    /// Invalid count with expected and actual.
+    #[error("invalid count, expected {expected} actual {actual}")]
+    InvalidCount {
+        /// Expected count
+        expected: usize,
+        /// Actual count
+        actual: usize
+    },
+    /// Validation failed.
+    #[error("validation fail")]
+    ValidationFail,
+    /// Message
+    #[error("{0}")]
+    Message(Cow<'static, str>),
+    /// There was an [std::io::Error].
+    #[error("io error {0}")]
+    Io(#[from] std::io::Error),
+    #[cfg(feature = "bevy")]
+    /// Async error
+    // #[error("async error {0}")]
+    // Async(#[from] bevy_defer::AccessError),
+    /// Promise error
+    #[error("promise error {0}")]
+    Promise(#[from] promise_out::Error),
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
