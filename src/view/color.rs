2a2eb816763736437f0f38f24e4eb6d6bc45f908
Shane Celis <shane.celis@gmail.com>
2024-07-31 16:39:05 -0400

    feature: Color confirm works.

--- src/view/color.rs
+++ src/view/color.rs
@@ -1,4 +1,5 @@
 use crate::construct::*;
+use super::*;
 use crate::{AskyState, Confirm, ConfirmState};
 use bevy::prelude::*;
 
@@ -43,7 +44,7 @@ pub(crate) fn confirm_view(
         ),
     >,
     mut question: Query<&mut Text, With<Question>>,
-    mut answers: Query<(&mut BackgroundColor, &mut Visibility, &Answer<bool>)>,
+    mut answers: Query<(&mut Text, &mut BackgroundColor, &mut Visibility, &Answer<bool>), Without<Question>>,
     color_view: Res<ColorView>,
 ) {
     for (mut state, confirm_state, children) in query.iter_mut() {
@@ -73,155 +74,47 @@ pub(crate) fn confirm_view(
                         );
                         text.sections[0].style = highlight;
 
-                        if matches!(asky_state, AskyState::Complete) {
-                            text.sections[3].value.replace_range(
-                                ..,
-                                match confirm_state.yes {
-                                    Some(true) => " Yes",
-                                    Some(false) => " No",
-                                    None => unreachable!(),
-                                },
-                            );
-                            text.sections[3].style.color = color_view.answer.into();
-                        } else {
-                            let (bg_no, bg_yes) = if confirm_state.yes.unwrap_or(false) {
-                                (color_view.lowlight, color_view.highlight)
-                            } else {
-                                (color_view.highlight, color_view.lowlight)
-                            };
-                        }
-
-                        if matches!(asky_state, AskyState::Complete) {
-                            text.sections[3].value.replace_range(
-                                ..,
-                                match confirm_state.yes {
-                                    Some(true) => "Yes",
-                                    Some(false) => "No",
-                                    None => unreachable!(),
-                                },
-                            )
-                        }
                     }
                     // for (mut background, mut visibility) in answers.iter_many_mut(children) {
-                    if let Ok((mut background, mut visibility, answer)) = answers.get_mut(*child) {
-                        if matches!(asky_state, AskyState::Complete) {
-                            *visibility = Visibility::Hidden;
-                        } else {
-                            let (bg_no, bg_yes) = if confirm_state.yes.unwrap_or(false) {
-                                (color_view.lowlight, color_view.highlight)
-                            } else {
-                                (color_view.highlight, color_view.lowlight)
-                            };
-                            if answer.0 {
-                                *background = bg_no.into();
-                            } else {
-                                *background = bg_yes.into();
+                    if let Ok((mut text, mut background, mut visibility, answer)) = answers.get_mut(*child) {
+                        let mut vis;
+                        match answer {
+                            Answer::Final => {
+                                vis = matches!(asky_state, AskyState::Complete);
+                                text.sections[0].value.replace_range(
+                                    ..,
+                                    if vis {
+                                        match confirm_state.yes {
+                                            Some(true) => "Yes",
+                                            Some(false) => "No",
+                                            None => "N/A",
+                                        }
+                                    } else {
+                                        ""
+                                    },
+                                )
+                            }
+                            Answer::Selection(yes) => {
+                                vis = ! matches!(asky_state, AskyState::Complete);
+                                if vis {
+                                    *background = if confirm_state.yes.map(|x| x == *yes).unwrap_or(false) {
+                                        color_view.highlight
+                                    } else {
+                                        color_view.lowlight
+                                    }.into();
+                                }
                             }
                         }
+                        *visibility = if vis { Visibility::Visible } else { Visibility::Hidden };
                     }
-
-                    // }
                 }
             }
         }
     }
 }
 
-// fn confirm_view2(
-//     mut query: Query<
-//         (Entity, &mut AskyState, &Confirm, &ConfirmState),
-//         Or<(Changed<AskyState>, Changed<ConfirmState>)>,
-//     >,
-//     color_view: Res<ColorView>,
-//     mut text: Query<&mut Text>,
-//     mut commands: Commands,
-// ) {
-//     for (id, mut state, confirm, confirm_state) in query.iter_mut() {
-//         match *state {
-//             AskyState::Frozen | AskyState::Uninit => (),
-//             ref asky_state => {
-//                 eprint!(".");
-//                 let highlight = TextStyle {
-//                     color: if matches!(asky_state, AskyState::Reading) {
-//                         color_view.highlight.into()
-//                     } else {
-//                         color_view.complete.into()
-//                     },
-//                     ..default()
-//                 };
-
-//                 let mut bundles = vec![TextBundle::from_sections([
-//                     TextSection::new(
-//                         format!(
-//                             "[{}] ",
-//                             match asky_state {
-//                                 AskyState::Reading => " ",
-//                                 AskyState::Complete => "x",
-//                                 AskyState::Error => "!",
-//                                 _ => unreachable!(),
-//                             }
-//                         ),
-//                         highlight.clone(),
-//                     ),
-//                     TextSection::new(confirm.message.as_ref(), TextStyle::default()),
-//                 ])];
-
-//                 if matches!(asky_state, AskyState::Complete) {
-//                     bundles.push(TextBundle::from_section(
-//                         match confirm_state.yes {
-//                             Some(true) => " Yes",
-//                             Some(false) => " No",
-//                             None => unreachable!(),
-//                         },
-//                     ));
-
-//                         TextStyle {
-//                             color: color_view.answer.into(),
-//                             ..default()
-//                         },
-//                 } else {
-//                     bundles.push(TextBundle::from_section(" ", TextStyle::default()));
-//                     let (bg_no, bg_yes) = if confirm_state.yes.unwrap_or(false) {
-//                         (color_view.lowlight, color_view.highlight)
-//                     } else {
-//                         (color_view.highlight, color_view.lowlight)
-//                     };
-
-//                     bundles.push(
-//                         TextBundle::from_section(" No ", TextStyle::default())
-//                             .with_background_color(bg_no.into()),
-//                     );
-//                     bundles.push(TextBundle::from_section(" ", TextStyle::default()));
-//                     bundles.push(
-//                         TextBundle::from_section(" Yes ", TextStyle::default())
-//                             .with_background_color(bg_yes.into()),
-//                     );
-//                 }
-//                 let new_child = commands
-//                     .spawn(NodeBundle::default())
-//                     .with_children(|parent| {
-//                         for b in bundles {
-//                             parent.spawn(b);
-//                         }
-//                     })
-//                     .id();
-//                 commands
-//                     .entity(id)
-//                     .despawn_descendants()
-//                     .replace_children(&[new_child]);
-//             }
-//         }
-//     }
-// }
-
 #[derive(Component)]
 pub struct View<T>(T);
-
-#[derive(Component)]
-pub struct Answer<T>(T);
-
-#[derive(Component)]
-pub struct Question;
 
 impl Construct for View<Confirm> {
     type Props = <Confirm as Construct>::Props;
@@ -255,25 +148,34 @@ impl Construct for View<Confirm> {
                             "[_] ".into(),                      // 0
                             confirm.message.to_string().into(), // 1
                             " ".into(),                         // 2
+                        ]),
+                        ..default()
+                    },
+                ));
+
+                parent.spawn((
+                    Answer::<bool>::Final,
+                    TextBundle {
+                        text: Text::from_sections([
                             TextSection::new(
                                 "",
                                 TextStyle {
                                     color: answer_color.into(),
                                     ..default()
                                 },
-                            ), // 3
+                            ),
                         ]),
                         ..default()
                     },
                 ));
                 parent.spawn((
-                    Answer(false),
+                    Answer::Selection(false),
                     TextBundle::from_section(" No ", TextStyle::default())
                         .with_background_color(bg_no.into()),
                 ));
                 parent.spawn(TextBundle::from_section(" ", TextStyle::default()));
                 parent.spawn((
-                    Answer(true),
+                    Answer::Selection(true),
                     TextBundle::from_section(" Yes ", TextStyle::default())
                         .with_background_color(bg_yes.into()),
                 ));
--- src/view/mod.rs
+++ src/view/mod.rs
@@ -1,3 +1,5 @@
+use bevy::prelude::*;
+
 pub mod ascii;
 mod button;
 pub mod click;
@@ -5,3 +7,13 @@ pub mod color;
 pub(crate) mod interaction;
 pub(crate) mod widget;
 pub use button::*;
+
+#[derive(Component)]
+pub struct Question;
+
+#[derive(Component)]
+// pub struct Answer<T>(T);
+pub enum Answer<T> {
+    Selection(T),
+    Final//(Option<T>)
+}
