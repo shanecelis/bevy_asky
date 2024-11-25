0d104dd3fb5903ce4cb06d65ded54c49b8421f3f
Shane Celis <shane.celis@gmail.com>
2024-11-25 08:00:19 -0500

    feature: Stick prompt_group back in.

--- src/sync.rs
+++ src/sync.rs
@@ -7,8 +7,7 @@ use std::fmt::Debug;
 pub trait AskyCommands {
     fn prompt<
         T: Construct + Bundle + Submitter,
-        V: Construct<Props = ()> + Bundle,
-    >(
+        V: Construct<Props = ()> + Bundle>(
         &mut self,
         props: impl Into<T::Props>,
         dest: impl Into<Dest>,
@@ -17,19 +16,19 @@ pub trait AskyCommands {
         <T as Construct>::Props: Send,
         <T as Submitter>::Out: Clone + Debug + Send + Sync;
 
-    // fn prompt_group<
-    //     T: Construct + Bundle + Part>(
-    //     &mut self,
-    //     group_prop: impl Into<<<T as Part>::Group as Construct>::Props>,
-    //     props: impl IntoIterator<Item = impl Into<T::Props>>,
-    //     dest: impl Into<Dest>,
-    // ) -> EntityCommands
-    // where
-    //     <T as Construct>::Props: Send,
-    //     <<T as Part>::Group as Construct>::Props: Send,
-    //     <T as Part>::Group: Bundle + Construct + Send + Sync,
-    //     <T as Part>::Group: Submitter,
-    //     <<T as Part>::Group as Submitter>::Out: Clone + Debug + Send + Sync;
+    fn prompt_group<
+        T: Construct + Bundle + Part, V: Construct<Props = ()> + Bundle>(
+        &mut self,
+        group_prop: impl Into<<<T as Part>::Group as Construct>::Props>,
+        props: impl IntoIterator<Item = impl Into<T::Props>>,
+        dest: impl Into<Dest>,
+    ) -> EntityCommands
+    where
+        <T as Construct>::Props: Send,
+        <<T as Part>::Group as Construct>::Props: Send,
+        <T as Part>::Group: Bundle + Construct + Send + Sync,
+        <T as Part>::Group: Submitter,
+        <<T as Part>::Group as Submitter>::Out: Clone + Debug + Send + Sync;
 }
 
 impl<'w, 's> AskyCommands for Commands<'w, 's> {
@@ -55,28 +54,31 @@ impl<'w, 's> AskyCommands for Commands<'w, 's> {
         commands
     }
 
-    // fn prompt_group<
-    //     T: Construct + Bundle + Part>(
-    //     &mut self,
-    //     group_prop: impl Into<<<T as Part>::Group as Construct>::Props>,
-    //     props: impl IntoIterator<Item = impl Into<T::Props>>,
-    //     dest: impl Into<Dest>,
-    // ) -> EntityCommands
-    // where
-    //     <T as Construct>::Props: Send,
-    //     <<T as Part>::Group as Construct>::Props: Send,
-    //     <T as Part>::Group: Bundle + Construct + Send + Sync + Submitter,
-    //     <<T as Part>::Group as Submitter>::Out: Clone + Debug + Send + Sync {
-    //     let d = dest.into();
+    fn prompt_group<T: Construct + Bundle + Part,
+                    V: Construct<Props = ()> + Bundle>(
+        &mut self,
+        group_prop: impl Into<<<T as Part>::Group as Construct>::Props>,
+        props: impl IntoIterator<Item = impl Into<T::Props>>,
+        dest: impl Into<Dest>,
+    ) -> EntityCommands
+    where
+        <T as Construct>::Props: Send,
+        <<T as Part>::Group as Construct>::Props: Send,
+        <T as Part>::Group: Bundle + Construct + Send + Sync + Submitter,
+        <<T as Part>::Group as Submitter>::Out: Clone + Debug + Send + Sync {
+        let d = dest.into();
 
-    //     let mut commands = d.entity(self);
-    //     commands
-    //         .construct::<T::Group>(group_prop)
-    //         .with_children(|parent| {
-    //             for prop in props.into_iter() {
-    //                 parent.construct::<T>(prop);
-    //             }
-    //         });
-    //     commands
-    // }
+        let mut commands = d.entity(self);
+        commands
+            .construct::<V>(())
+            .construct::<T::Group>(group_prop)
+            .with_children(|parent| {
+                for prop in props.into_iter() {
+                    parent
+                        .construct::<V>(())
+                        .construct::<T>(prop);
+                }
+            });
+        commands
+    }
 }
